a77b8e5046bbff3934a097826c7a4fe11355fc8d
dulsara <dulsaramanupriyal@gmail.com>
2022-05-13 13:42:46 +0530

    fixing security issues with user

--- src/main/java/com/dulsara/ewallet/ewalletapp/security/service/MyUserDetailsService.java
+++ src/main/java/com/dulsara/ewallet/ewalletapp/security/service/MyUserDetailsService.java
@@ -13,7 +13,7 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-            return new User("foo", "foo",
+            return new User("user", "user",
                     new ArrayList<>());
     }
 }
